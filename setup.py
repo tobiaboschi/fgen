d749f406d8bfc3f474bb49f74a7d63a9d68b186c
Tobia Boschi <tobiboschi@me.com>
2021-10-26 20:16:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 
 
 setup(name="fgen",
-      packages=["fgen"],
+      packages=["fgen_solver"],
       install_requires=["numpy", "scikit-learn", "scikit-fda", "tqdm", "scipy", "matplotlib", "pandas"])
